c7b2584a29e417e334e15aa7bff080724fd7a5eb
lalitha679 <rs200679@rguktsklm.ac.in>
2025-10-12 22:40:10 +0530

    final commit

--- Backend/server.js
+++ Backend/server.js
@@ -1,22 +1,43 @@
-// server.js
 import express from 'express';
 import cors from 'cors';
 import "dotenv/config";
 import connectDB from './configs/db.js';
 import { clerkMiddleware } from '@clerk/express';
 
-
 import clerkWebhooks from './controllers/clerkWebhooks.js';
 import userRouter from './routes/userRoutes.js';
-await connectDB();
 
+import hotelRouter from './routes/hotelRoutes.js';
+import connectCloudinary from './configs/cloudinary.js';
+import roomRouter from './routes/roomRoutes.js';
+import bookingRouter from './routes/bookingRoutes.js';
+import { stripeWebhook } from './controllers/stripeWebhooks.js';
+
+await connectDB();
+await connectCloudinary();
 
 const app = express();
 
-app.use(cors())
+app.use(cors({
+    origin: function(origin, callback){
+      // allow requests with no origin like mobile apps or curl requests
+      if(!origin) return callback(null, true);
+      const vercelOriginPattern = /\.vercel\.app$/;
+      if(vercelOriginPattern.test(origin)){
+        return callback(null, true);
+      }
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    },
+    credentials: true
+  }))
+
+//Stripe webhook
+app.post('/api/stripe', express.raw({type: 'application/json'}), stripeWebhook);
 
 // ✅ After raw middleware, now apply json parsing for other routes
 app.use(express.json());
+
 
 // Clerk middleware
 app.use(clerkMiddleware());
@@ -25,14 +46,14 @@ app.use(clerkMiddleware());
 app.use('/api/clerk',clerkWebhooks);
 
 
-
 // Health check
 app.get('/', (req, res) => {
     res.send('QuickStay server is live!');
 });
-
-app.use('/api/user',userRouter)
-
+app.use('/api/user', userRouter);
+app.use('/api/hotels', hotelRouter);
+app.use('/api/rooms', roomRouter);
+app.use('/api/bookings', bookingRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
